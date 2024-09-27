08db69323f7325b960d4f5fb9df6c30f159f8c53
노현수 <kevinsoo1014@gmail.com>
2024-09-27 14:56:22 +0900

    이미지 파일 추가

--- 서버 프로그래밍/4주 차/myContacts/app.js
+++ 서버 프로그래밍/4주 차/myContacts/app.js
@@ -1,17 +1,32 @@
 // URL: http://localhost:3000/
 // express 모듈을 사용하여 서버를 실행
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
+
+// 이미지가 안뜨는 문제가 발생하여 아래 링크를 참고하여 해결
+// https://als2019.tistory.com/137 참고
+// 이미지 요청 처리
+// 이미지가 있는 폴더를 public 폴더로 설정하고, 이미지 요청이 들어오면 해당 이미지를 응답으로 전송
+var router = express.Router();
+app.use(express.static('public'));
+
+router.get('/images/:imageName', function (req, res) {
+    var imgName = req.params.imageName;
+    console.log('이미지 요청: ' + imgName);
+    res.sendFile('/public/image/' + imgName);
+});
 
 app.get("/", (req, res) => {
     res.status(200);
     res.send("Hello, Node!");
 });
 
-// 모든 연락처 가져오기
+// 모든 연락처 가져오기, index.html 파일을 보여줌
 app.get("/contacts", (req, res) => {
-    res.status(200).send("Contacts Page");
+    // res.status(200).send("Contacts Page");
+    res.sendFile(__dirname + "/index.html");
 });
 
 // 새 연락처 생성
@@ -33,6 +48,16 @@ app.put("/contacts/:id", (req, res) => {
 app.delete("/content/:id", (req, res) => {
     res.status(200).send(`Delate Contact for ID: ${req.params.id}`);
 });
+
+// app.get("/", (req, res) => {
+//     const headers = req.headers;
+//     res.send(headers);
+// });
+
+// app.get("/", (req, res) => {
+//     res.status(200);
+//     res.send("Hello, Node!");
+// });
 
 // 서버 실행
 app.listen(port, () => {
