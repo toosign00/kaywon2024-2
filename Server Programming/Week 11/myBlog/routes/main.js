bc2278c54b09bd4b9c2549a5e3caf5892d2dbd38
toosign <kevinsoo1014@gmail.com>
2024-11-21 09:07:53 +0900

    서버 프로그래밍 11주 차

--- Server Programming/Week 11/myBlog/routes/main.js
+++ Server Programming/Week 11/myBlog/routes/main.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asynchandler = require("express-async-handler");
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// "/home" 라우트
+router.get(
+  ["/", "/home"],
+  asynchandler(async (req, res) => {
+    const locals = {
+      title: "Home",
+    };
+    const data = await Post.find();
+    res.render("index", {locals, data, layout: mainLayout});
+  })
+);
+
+// GET post/:id
+// 게시물 상세 보기
+
+router.get(
+  "/post/:id",
+  asynchandler(async (req, res) => {
+    const data = await Post.findOne({_id: req.params.id});
+    res.render("post", {data, layout: mainLayout});
+  })
+);
 
 module.exports = router;
